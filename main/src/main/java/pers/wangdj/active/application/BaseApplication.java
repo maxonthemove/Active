ecd416f85add865d815ff0e03960fa56251a35bf
王东杰 <wangdja@yonyou.com>
2018-04-27 22:50:30 +0800

    唤醒功能完成

--- main/src/main/java/pers/wangdj/active/application/BaseApplication.java
+++ main/src/main/java/pers/wangdj/active/application/BaseApplication.java
@@ -4,6 +4,8 @@ import android.app.Application;
 import android.content.Context;
 import android.support.multidex.MultiDex;
 
+import com.iflytek.cloud.SpeechConstant;
+import com.iflytek.cloud.SpeechUtility;
 import com.tencent.bugly.crashreport.CrashReport;
 
 import pers.wangdj.active.utils.Constants;
@@ -24,6 +26,9 @@ public class BaseApplication extends Application {
         super.onCreate();
         //初始化Bugly
         CrashReport.initCrashReport(getApplicationContext(), Constants.BUGLY_APP_ID, true);
+        // 将“12345678”替换成您申请的APPID，申请地址：http://www.xfyun.cn
+        // 请勿在“=”与appid之间添加任何空字符或者转义符
+        SpeechUtility.createUtility(this, SpeechConstant.APPID +"=5ae28f23");
 
     }
 
